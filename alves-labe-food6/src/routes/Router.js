f270aab357c7c7b2f85de5b4bbb6259557d2c970
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-08-04 17:18:40 -0300

    edit Address em processo

--- alves-labe-food6/src/routes/Router.js
+++ alves-labe-food6/src/routes/Router.js
@@ -8,7 +8,7 @@ import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import Address from '../pages/Address';
 import GlobalState from '../components/global/GlobalState';
-
+import { EditAddress } from '../pages/editAddress';
 const Router = () => {
   return (
     <BrowserRouter>
@@ -16,7 +16,8 @@ const Router = () => {
         <Routes>
           <Route path='/login' element={<Login />}></Route>     
           <Route path='/signup' element={<Signup />}></Route>  
-          <Route path='/address' element={<Address />}></Route>        
+          <Route path='/address' element={<Address />}></Route>  
+          <Route path='/editaddress' element={<EditAddress />}></Route>        
         </Routes>
       </GlobalState>
     </BrowserRouter>
