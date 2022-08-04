4d5c12c5598e3da086b8b0e50bd23be395c07bff
Arthur Grossi Fernandes <arthur17000@gmail.com>
2022-08-04 17:47:06 -0300

    Atualização Home

--- alves-labe-food6/src/routes/Router.js
+++ alves-labe-food6/src/routes/Router.js
@@ -9,6 +9,7 @@ import Signup from '../pages/Signup';
 import Address from '../pages/Address';
 import GlobalState from '../components/global/GlobalState';
 import HomePage from '../pages/homePage/HomePage';
+import RestaurantPage from '../pages/RestaurantPage/RestaurantPage';
 
 const Router = () => {
   return (
@@ -18,7 +19,8 @@ const Router = () => {
           <Route path='/login' element={<Login />}></Route>     
           <Route path='/signup' element={<Signup />}></Route>  
           <Route path='/address' element={<Address />}></Route>    
-          <Route path='/' element={<HomePage />}></Route>        
+          <Route path='/' element={<HomePage />}></Route>   
+          <Route path='/restaurant/:id' element={<RestaurantPage />}></Route>   
         </Routes>
       </GlobalState>
     </BrowserRouter>
